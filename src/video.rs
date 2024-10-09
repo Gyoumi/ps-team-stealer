e534e04bbfeac930582c72eff22c892507b2efde
gyumi <pokemonigt@gmail.com>
2024-10-10 00:29:12 +1100

    started branch

--- src/video.rs
+++ src/video.rs
@@ -1,76 +1,141 @@
-use rustube::{Error, Id, Stream, Video, VideoDescrambler, VideoFetcher};
-use rustube::fetcher::{recommended_cookies, recommended_headers};
-// use reqwest::Client;
-// use tokio_stream::StreamExt;
+use tokio::process::Command;
+use tokio::io::AsyncReadExt;
+use core::str;
+use std::process::Stdio;
+use image::ImageBuffer;
+
+struct Resolution {
+    width: u32,
+    height: u32,
+    capacity: usize
+}
+
+type FrameBuffer = ImageBuffer<image::Rgb<u8>, Vec<u8>>;
+
+pub async fn start(url: &str) -> Result<(), Box<dyn std::error::Error>>{
+    let resolution = get_resolution(url).await?;
+    println!("resolution is {}x{}", resolution.width, resolution.height);
+
+    let video_stream = stream_youtube_video(url)?;
+    process_video(video_stream, resolution).await?;
+    Ok(())
+}
+
+async fn get_resolution(url: &str) -> Result<Resolution, std::io::Error> {
+    let yt_dlp_process = Command::new("yt-dlp")
+        .arg("-f")
+        .arg("bv")
+        .arg("-o")
+        .arg("-")
+        .arg(url)
+        .arg("--skip-download")
+        .arg("--print")
+        .arg("resolution")
+        .stderr(Stdio::piped())
+        .spawn()?;
+
+    let output = yt_dlp_process.wait_with_output().await.expect("error yt-dlp ffmpeg task");
+    let stdout = String::from_utf8(output.stderr).map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?; 
+    println!("exit status was: {}", stdout);
+    let parts: Vec<&str> = stdout.trim().split('x').collect();
+
+    let width: u32 = parts[0].parse().unwrap();
+    let height: u32 = parts[1].parse().unwrap();
+
+    let capacity: usize =  (width * height * 3).try_into().unwrap();
+    
+    Ok(Resolution {
+        width, height, capacity
+    })
+}
+
+fn stream_youtube_video(url: &str) -> Result<Stdio, std::io::Error> {
+    let mut yt_dlp_process = Command::new("yt-dlp")
+        .arg("-f")
+        .arg("bv")
+        .arg("-o")
+        .arg("-")
+        .arg(url)
+        .stdout(Stdio::piped())
+        .spawn()?;
+
+    let stdout: Stdio = yt_dlp_process.stdout.take().unwrap().try_into().unwrap();
+
+    tokio::spawn(async move {
+        let status = yt_dlp_process.wait().await.expect("error yt-dlp ffmpeg task");
+        println!("exit status was: {}", status);
+    });
 
 
-pub type Result<T, E = std::io::Error> = core::result::Result<T, E>;
+    Ok(stdout)
+}
 
-// pub async fn download_video(video_url: &str) {
-//     println!("starting download for {video_url}");
-//     println!("downloaded video to {:?}", rustube::download_best_quality(&video_url).await.unwrap());
-// }
+async fn process_video(video_stream: Stdio, frame_size: Resolution) -> Result<Vec<u8>, std::io::Error> {
+    let mut ffmpeg_process = Command::new("ffmpeg")
+    .arg("-i")
+    .arg("-")
+    .arg("-vf")
+    .arg("fps=18")
+    .arg("-f")
+    .arg("image2pipe")
+    .arg("-pix_fmt")
+    .arg("rgb24")
+    .arg("-vcodec")
+    .arg("rawvideo")
+    .arg("-")
+    .stdin(video_stream)
+    .stdout(Stdio::piped())
+    .spawn()?;
+    let mut ffmpeg_stdout = ffmpeg_process.stdout.take().unwrap(); 
 
-pub async fn video(video_url: &str) -> Result<Vec<u8>> {
-    let id = Id::from_raw(video_url).unwrap();
+    tokio::spawn(async move {
+        let status = ffmpeg_process.wait().await.expect("error finishing ffmpeg task");
+        println!("exit status was: {}", status);
+    });
 
-    // let cookie_jar: reqwest::cookie::Jar = recommended_cookies();
-    // let headers: reqwest::header::HeaderMap = recommended_headers();
+    let mut buffer = Vec::new();
+    let mut buffer_idx = 0;
+    let mut remainder = 0;
 
-    // let client = Client::builder()
-    //         .default_headers(headers)
-    //         .cookie_provider(std::sync::Arc::new(cookie_jar))
-    //         .build().map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, format!("error building client: {e}")))?;
+    let mut frame_number = 0;
 
-    let fetcher: VideoFetcher = VideoFetcher::from_id(id.into_owned()).map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, format!("error descrambling video: {e}")))?;
-    let descrambler: VideoDescrambler = fetcher.fetch().await.map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, format!("error fetching descrambler: {e}")))?;
-    let video: Video = descrambler.descramble().map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, format!("error descrambling video: {e}")))?;
+    loop {
+        let mut line = [0; 131972];
+        let n = ffmpeg_stdout.read(&mut line).await?;
 
+        if n == 0 {
+            println!("end of file reached! buffer size is {}", buffer.len());
+            break;
+        }
 
-    let video_path = video
-        .streams()
-        .iter()
-        .filter(|stream| stream.includes_video_track && stream.quality == rustube::video_info::player_response::streaming_data::Quality::Hd720)
-        .max_by_key(|stream| stream.quality_label)
-        .unwrap()
-        .download()
-        .await.map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, format!("error downloading video: {e}")))?;
-    //let stream: &Stream = video.best_video().unwrap();
-    // let video_info = descrambler.video_info();
-    // println!("title of video: {:?}", video_info);
-    // let path = stream.download().await.map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, format!("error downloading video: {e}")))?;
-    // println!("downloaded to {:?}", path);
+        buffer.extend_from_slice(&line[..n]);      
+        if remainder + n >= frame_size.capacity {
+            let curr_bytes = buffer[buffer_idx..buffer_idx+frame_size.capacity].to_vec();
+            
+            tokio::spawn(async move {
+                let img = FrameBuffer::from_raw(frame_size.width, frame_size.height, curr_bytes).unwrap();
+                process_frames(img, frame_number).expect("image processing failed");
+            });
 
-    //Ok(String::from(stream.signature_cipher.url.as_str()))
+            frame_number+=1;  
 
-    // let res = get(client, &stream.signature_cipher.url).await.map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, format!("error fetching video url: {e}")))?;
-    // let bytes = get_stream_bytes(res.bytes_stream()).await?;
-    // Ok(bytes)
+            buffer_idx += frame_size.capacity;
+            remainder -= std::cmp::min(frame_size.capacity, remainder);
+        } else {
+            remainder += n;
+        }        
+    }
 
     Ok(Vec::new())
 }
 
+fn process_frames(frame: FrameBuffer, frame_number: i32) -> Result <(), std::io::Error> {
 
-// // copied from rustube repo
-// async fn get(client: Client, url: &url::Url) -> Result<reqwest::Response, Error> {
-//     Ok(
-//         client
-//             .get(url.as_str())
-//             .send()
-//             .await?
-//             .error_for_status()?
-//     )
-// }
+    let frame_path = format!("./frames/frame_{}.bmp", frame_number); // Save the first frame
 
-// async fn get_stream_bytes<S>(mut stream: S) -> Result<Vec<u8>>
-// where
-//     S: tokio_stream::Stream<Item=reqwest::Result<bytes::Bytes>> + Unpin
-//     {
-//         let mut buffer = Vec::new();
 
-//         while let Some(chunk) = stream.next().await {
-//             let chunk = chunk.map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, format!("error retrieving chunk: {e}")))?;
-//             buffer.extend_from_slice(&chunk);
-//         }
-//         Ok(buffer)
-// }
+    frame.save(frame_path).expect("unable to save image");
+
+    println!("Frame {}", frame_number);
+    Ok(())
+}
