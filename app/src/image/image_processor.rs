00eac3f43cd3633b9ca887af199a169e2ace2975
gyumi <pokemonigt@gmail.com>
2025-07-12 04:10:58 +1000

    committing code for ort rc.10

--- app/src/image/image_processor.rs
+++ app/src/image/image_processor.rs
@@ -2,12 +2,12 @@ use image::DynamicImage;
 use super::image_segmenter;
 
 pub async fn process_image(id: usize) {
-    let segments = image_segmenter::segment_image(id).await;
-    match segments {
-        Ok(images) => for (i, img) in images.iter().enumerate() {
-            let seg_path = format!("./segment/seg_{}.png", i); 
-            img.save(seg_path).expect("unable to save image");
-        },
-        Err(e) => eprintln!("{}", e)
-    }
+    let _segments = image_segmenter::segment_image(id);
+    // match segments {
+    //     Ok(images) => for (label, img) in &images {
+    //         let seg_path = format!("./segment/seg_{}.png", label); 
+    //         img.save(seg_path).expect("unable to save image");
+    //     },
+    //     Err(e) => eprintln!("{}", e)
+    // }
 }
