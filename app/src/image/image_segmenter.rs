5fc645f308c0776d9bf596de6a7342435eda7835
gyumi <pokemonigt@gmail.com>
2025-07-15 03:52:01 +1000

    finished image segmenter

--- app/src/image/image_segmenter.rs
+++ app/src/image/image_segmenter.rs
@@ -7,6 +7,7 @@ use imageproc::drawing::draw_text_mut;
 
 use ndarray::ArrayViewD;
 
+#[derive(Clone, Debug, Copy)]
 struct BoundingBox {
     x1: f32,
     y1: f32,
@@ -15,8 +16,20 @@ struct BoundingBox {
     class_id: usize,
     confidence: f32,
 }
+impl BoundingBox {
+    fn new(x1: f32, y1: f32, x2: f32, y2: f32, class_id: usize, confidence: f32) -> Self {
+        Self { x1, y1, x2, y2, class_id, confidence }
+    }
+    fn intersection(&self, bx: &BoundingBox) -> f32 {
+        (self.x2.min(bx.x2) - self.x1.max(bx.x1)) * (self.y2.min(bx.y2) - self.y1.max(bx.y1))
+    }
+    
+    fn union(&self, bx: &BoundingBox) -> f32 {
+        ((self.x2 - self.x1) * (self.y2 - self.y1)) + ((bx.x2 - bx.x1) * (bx.y2 - bx.y1))
+            - self.intersection(bx)
+    }
+}
 
-/// Convert an RgbImage to a CHW float32 tensor normalized to 0-1.
 fn rgb_image_to_tensor(image: RgbImage, width: u32, height: u32) -> Result<(Array<f32, Ix4>, u32, u32), ModelError> {
     let (og_width, og_height) = (image.width(), image.height());
     let resized = resize(&image, width, height, FilterType::CatmullRom);
@@ -36,18 +49,18 @@ fn rgb_image_to_tensor(image: RgbImage, width: u32, height: u32) -> Result<(Arra
     Ok((tensor, og_width, og_height))
 }
 
-/// Draw bounding boxes on an RgbImage.
-fn draw_boxes(mut img: RgbImage, boxes: &[BoundingBox]) -> RgbImage {
+fn draw_boxes(mut img: RgbImage, boxes: &[BoundingBox], height: u32, width: u32) -> RgbImage {
     for bbox in boxes {
         let color = Rgb([255, 0, 0]);
         for x in bbox.x1 as u32..bbox.x2 as u32 {
-            if (bbox.y1 as u32) >= 0 && (bbox.y1 as u32) < img.height() {
+            if x >= 0 && x < width && (bbox.y1 as u32) >= 0 && (bbox.y1 as u32) < height && (bbox.y2 as u32) >= 0 && (bbox.y2 as u32) < height {
                 img.put_pixel(x, bbox.y1 as u32, color);
                 img.put_pixel(x, bbox.y2 as u32, color);
             }
         }
+
         for y in bbox.y1 as u32..bbox.y2 as u32 {
-            if (bbox.x1 as u32) >= 0 && (bbox.x1 as u32) < img.width() {
+            if y >= 0 && y < height && (bbox.x1 as u32) >= 0 && (bbox.x1 as u32) < width && (bbox.x2 as u32) >= 0 && (bbox.x2 as u32) < width {
                 img.put_pixel(bbox.x1 as u32, y, color);
                 img.put_pixel(bbox.x2 as u32, y, color);
             }
@@ -56,44 +69,7 @@ fn draw_boxes(mut img: RgbImage, boxes: &[BoundingBox]) -> RgbImage {
     img
 }
 
-/// Simple postprocessing: convert YOLOv8 output to boxes.
-fn postprocess_output(output: &ArrayViewD<f32>, img_width: u32, img_height: u32) -> Vec<BoundingBox> { //(x1, y1, x2, y2, class_id, confidence)
-    // let shape = output.shape();
-    // let num_classes = shape[1] - 5;
-    // let num_anchors = shape[2];
-    // let mut boxes = Vec::new();
-    // let confidence_threshold = 0.05;
-
-    // for anchor in 0..num_anchors {
-    //     let x = output[[0, 0, anchor]];
-    //     let y = output[[0, 1, anchor]];
-    //     let w = output[[0, 2, anchor]];
-    //     let h = output[[0, 3, anchor]];
-    //     let obj_conf = output[[0, 4, anchor]];
-
-    //     // Class scores
-    //     let mut best_class = 0;
-    //     let mut best_score = 0.0;
-    //     for class_idx in 0..num_classes {
-    //         let score = output[[0, 5 + class_idx, anchor]];
-    //         if score > best_score {
-    //             best_score = score;
-    //             best_class = class_idx;
-    //         }
-    //     }
-
-    //     let confidence = obj_conf * best_score;
-
-    //     if confidence > confidence_threshold {
-    //         let x1 = x - w / 2.0;
-    //         let y1 = y - h / 2.0;
-    //         let x2 = x + w / 2.0;
-    //         let y2 = y + h / 2.0;
-    //         boxes.push((x1, y1, x2, y2, best_class, confidence));
-    //     }
-    // }
-
-    // boxes
+fn postprocess_output(output: &ArrayViewD<f32>, img_width: u32, img_height: u32) -> Vec<BoundingBox> { 
     let mut boxes = Vec::new();
         let output = output.slice(s![.., .., 0]);
 
@@ -107,7 +83,7 @@ fn postprocess_output(output: &ArrayViewD<f32>, img_width: u32, img_height: u32)
                 .reduce(|accum, row| if row.1 > accum.1 { row } else { accum })
                 .unwrap();
 
-            if prob < 0.25 {
+            if prob < 0.4 {
                 continue;
             }
 
@@ -116,45 +92,67 @@ fn postprocess_output(output: &ArrayViewD<f32>, img_width: u32, img_height: u32)
             let w = row[2] / 640. * (img_width as f32);
             let h = row[3] / 640. * (img_height as f32);
 
-            boxes.push(BoundingBox {
-                x1: xc - w / 2.,
-                y1: yc - h / 2.,
-                x2: xc + w / 2.,
-                y2: yc + h / 2.,
-                class_id: class_id.try_into().unwrap(),
-                confidence: prob,
-            });
+            boxes.push(BoundingBox::new(
+                xc - w / 2.,
+                yc - h / 2.,
+                xc + w / 2.,
+                yc + h / 2.,
+                class_id.try_into().unwrap(),
+                prob,
+            ));
         }
 
-    boxes
+        let mut sorted = boxes.clone();
+        sorted.sort_by(|a, b| b.confidence.partial_cmp(&a.confidence).unwrap());
+        let mut res = Vec::new();
+        
+        while !sorted.is_empty() {
+            let best = sorted.remove(0);
+            res.push(best);
+        
+            sorted.retain(|b| {
+                if b.class_id != best.class_id {
+                    return true;
+                }
+        
+                let inter = best.intersection(b);
+                let union = best.union(b);
+        
+                if inter <= 0.0 || union <= 0.0 {
+                    return true; 
+                }
+        
+                let iou = inter / union;
+        
+                iou < 0.5 
+            });
+        }
+        
+        res
+        
 }
 
 
-pub fn segment_image(id: usize) -> Result<RgbImage, ModelError> { //Result<HashMap<String, RgbImage>, ModelError> {
-    let image = image::open("frame_50.png")?.to_rgb8();
-
-
+pub fn segment_image(id: usize, image: RgbImage) -> Result<RgbImage, ModelError> { //Result<HashMap<String, RgbImage>, ModelError> {
+    println!("Segmenting image {}", id);
+    let og_image = image.clone();
     let (input_tensor, og_width, og_height) = rgb_image_to_tensor(image, 640, 640)?;
     let session = Session::builder()?
     .with_optimization_level(GraphOptimizationLevel::Level3)?
-    .commit_from_file("src/image/model/yolo_custom.onnx")?;
+    .commit_from_file("src/image/model/75epoch-640imgs-tourhover-last.onnx")?;
 
     let input = ort::inputs![input_tensor.view()]?;
     let outputs: SessionOutputs = session.run(input)?;
 
-    // Print raw model output
     let output_array = outputs["output0"].try_extract_tensor::<f32>().unwrap().t().into_owned();
     println!("Shape: {:?}", output_array.view().shape());
 
 
-    // Postprocess boxes
     let boxes = postprocess_output(&output_array.view(), og_width, og_height);
     println!("Detected {} boxes", boxes.len());
 
-    // Draw and save
-    let og_image = image::open("frame_50.png")?.to_rgb8();
-    let output_img = draw_boxes(og_image, &boxes);
-    output_img.save("segment/segmented.png")?;
+    let output_img = draw_boxes(og_image, &boxes, og_height, og_width);
+    output_img.save(format!("segment/segmented_{}.png", id))?;
 
     Ok(output_img)
 }
