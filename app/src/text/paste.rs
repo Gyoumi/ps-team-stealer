8792521503551a70b9ecdd219228aced9f140265
gyumi <pokemonigt@gmail.com>
2025-07-25 08:12:42 +1000

    testing pasting

--- app/src/text/paste.rs
+++ app/src/text/paste.rs
@@ -2,28 +2,32 @@ use reqwest::header::CONTENT_TYPE;
 use std::collections::HashMap;
 
 pub async fn upload_team(team: String) {
-    let client = reqwest::Client::new();
+    let client = reqwest::Client::builder()
+        .redirect(reqwest::redirect::Policy::none())
+        .build()
+        .unwrap();
 
     let mut params = HashMap::new();
     params.insert("paste", team);
 
     let res = client
-        .post("https://pokepast.es/create")
+        .post("https://pokepasat.es/create")
         .header(CONTENT_TYPE, "application/x-www-form-urlencoded")
         .form(&params)
         .send()
         .await;
 
     if let Ok(res) = res {
-        let location = res
-            .headers()
-            .get("Location")
-            .and_then(|l| l.to_str().ok())
-            .map(|s| s.to_string());
-
-        let text = res.text().await.unwrap();
-        println!("{}", text);
-        println!("Location: {:?}", location.unwrap());
+        if res.status().is_redirection() {
+            let location = res
+                .headers()
+                .get("Location")
+                .and_then(|l| l.to_str().ok())
+                .map(|s| format!("https://pokepasat.es{}", s));
+            println!("Paste URL: {:?}", location);
+        } else {
+            println!("Unexpected response: {:?}", res.status());
+        }
     } else {
         println!("Error: {}", res.unwrap_err());
     }
